d615e8a700c98265478eafe60ea45e53545080c9
hktrollgames <hktrollgames@gmail.com>
2022-07-17 13:47:19 +0900

    충돌 테스트

--- Assets/ClickTest.cs
+++ Assets/ClickTest.cs
@@ -1,18 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClickTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform pointer;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo)) // Physics물리 -> 충돌 영역 필요
+            {
+                print(hitInfo.point);
+                pointer.position = hitInfo.point;
+            }
+        }
     }
 }
